3e619d2a2886c861fc5ed5e2cb7f0eb114f371ff
Felipe Rocha <felipe90rcha@hotmail.com>
2025-09-10 19:38:32 -0300

    fix(ajustes): configuracao cors

--- app/main.py
+++ app/main.py
@@ -10,6 +10,7 @@ from app.database import engine, Base
 from app.models.user import User
 from app.models.tutor import Tutor
 from app.models.pet import Pet
+from fastapi.middleware.cors import CORSMiddleware
 
 # Base.metadata.create_all(bind=engine)
 
@@ -17,6 +18,22 @@ app = FastAPI(
     title="Austay API",
     description="API para gestão de pets e tutores.",
     version="0.1.0",
+)
+
+# 2. Defina de quais origens você quer aceitar requisições
+#    Neste caso, o seu frontend React
+origins = [
+    "http://localhost:5173",
+    # Você pode adicionar outras URLs aqui, como a do seu site em produção
+]
+
+# 3. Adicione o middleware ao seu app FastAPI
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Permite as origens definidas acima
+    allow_credentials=True,
+    allow_methods=["*"],  # Permite todos os métodos (GET, POST, etc.)
+    allow_headers=["*"],  # Permite todos os cabeçalhos
 )
 
 app.include_router(user_controller.router)
